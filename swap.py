dd531bd932032141abcb6bc0cde367c657dbad81
up-2021 <upravljanje.projektima.2021@gmail.com>
2021-03-21 19:12:03 +0100

    xor swap implemented

--- swap.py
+++ swap.py
@@ -1,9 +1,17 @@
 import sys
 
 
+def xor_swap(a,b):
+    a = a ^ b
+    b = a ^ b
+    a = a ^ b
+    return a,b
+
 if __name__=='__main__':
-    x = sys.argv[1]
-    y = sys.argv[2]
+    x = int(sys.argv[1])
+    y = int(sys.argv[2])
+
+    x,y = xor_swap(a,b)
 
     print(x)
     print(y)

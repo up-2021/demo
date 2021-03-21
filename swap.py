55b294f4fe545482b52fd8dec95d1ea4fc02b991
up-2021 <upravljanje.projektima.2021@gmail.com>
2021-03-21 19:09:18 +0100

    defined temp_swap method for swapping numbers

--- swap.py
+++ swap.py
@@ -1,9 +1,16 @@
 import sys
 
+def temp_swap(a,b):
+    temp = a
+    a = b
+    b = a
+    return a,b
 
 if __name__=='__main__':
-    x = sys.argv[1]
-    y = sys.argv[2]
+    x = int(sys.argv[1])
+    y = int(sys.argv[2])
+
+    x,y = temp_swap(x,y)
 
     print(x)
     print(y)
